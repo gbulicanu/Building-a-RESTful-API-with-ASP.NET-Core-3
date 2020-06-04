24da5aa1ffc9fb8f584503c7154b2c05368491e1
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-06-04 17:39:35 +0300

    Add courses automapper profile

---
+++ Profiles/CoursesProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+using CourseLibrary.API.Helpers;
+
+namespace CourseLibrary.API.Profiles
+{
+    public class CoursesProfile : Profile
+    {
+        public CoursesProfile()
+        {
+            CreateMap<Entities.Course, Models.CourseDto>();
+        }
+    }
+}
