c487d4fdc39c73823f1985c0c5d2bcfb911b2ff6
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-06-04 17:32:58 +0300

    Add CoursesController skeleton

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+
+using CourseLibrary.API.Services;
+
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors/{authorId}/courses")]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository courseLibraryRepository;
+        private readonly IMapper mapper;
+
+        public CoursesController(
+            ICourseLibraryRepository courseLibraryRepository,
+            IMapper mapper)
+        {
+            this.courseLibraryRepository = courseLibraryRepository
+                ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+            this.mapper = mapper
+                ?? throw new ArgumentNullException(nameof(mapper));
+        }
+    }
+}
