eb84dd063568303e0f61cede64016e51bb923be5
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-06-08 17:01:49 +0300

    Add support for creating collection of resourses

---
+++ Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository courseLibraryRepository;
+        private readonly IMapper mapper;
+
+        public AuthorCollectionsController(
+            ICourseLibraryRepository courseLibraryRepository,
+            IMapper mapper)
+        {
+            this.courseLibraryRepository = courseLibraryRepository
+                ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+            this.mapper = mapper
+                ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<AuthorDto>> CreateAuthorCollection(IEnumerable<AuthotForCreateDto> authors) 
+        {
+            var authorEntities = this.mapper.Map<IEnumerable<Entities.Author>>(authors);
+            foreach (var author in authorEntities) 
+            {
+                this.courseLibraryRepository.AddAuthor(author);
+            }
+
+            this.courseLibraryRepository.Save();
+
+            return Ok();
+        }
+    }
+}
